729b71a5f61c24a728a65a47569f6b319db19d85
Sid Jr <sidalbertok@gmail.com>
2026-03-24 17:03:16 -0300

    Create user-repository.ts

---
+++ Tecnico em Desevolvimento de Sistemas/UC11 - Testes/Teste_banco/user-repository.ts
@@ -0,0 +1,26 @@
+import { promises } from "dns";
+import { conn } from "../database";
+import { Usuario } from "../model/user";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
+import { RequestHandler } from "express";
+
+export class UsuarioReposity{
+     async mostrarTodos():Promise<Usuario[]>{
+        const [resultado] = await conn.query<RowDataPacket[]>("SELECT * FROM minha_tabela"); 
+        return resultado.map((u) => new Usuario(u.id,u.idade,u.nome))
+     }
+     async inserir(nome:string,idade:string):Promise<Usuario>{
+         const [resultado2] =  await conn.execute<ResultSetHeader>("INSERT INTO minha_tabela (nome,idade) values (?,?)",[nome,idade]);
+         return new Usuario(resultado2.insertId,nome,idade);
+     }
+    async delete(id:Number):Promise<ResultSetHeader>{
+         const [resultado3] =  await conn.query<ResultSetHeader>("DELETE FROM minha_tabela WHERE id = ?",[id]);
+         return resultado3;
+     }
+     async update(id:Number):Promise<ResultSetHeader>{
+         const [resultado4] =  await conn.query<ResultSetHeader>("UPDATE minha_tabela SET nome = ?, idade = ? WHERE id = ?",[id]);
+         return resultado4;
+     }
+
+}
+       
