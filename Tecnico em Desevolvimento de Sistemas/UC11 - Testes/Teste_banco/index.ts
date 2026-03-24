d2bd748747c4bb327ac0d348ea2e09a8c76b1221
Sid Jr <sidalbertok@gmail.com>
2026-03-24 17:02:59 -0300

    Create index.ts

---
+++ Tecnico em Desevolvimento de Sistemas/UC11 - Testes/Teste_banco/index.ts
@@ -0,0 +1,11 @@
+import { UsuarioReposity } from "./config/repositories/user-repository";
+
+async function main() {
+    const userRepo = new UsuarioReposity()
+    console.log(await userRepo.inserir("Daniel","20"));
+    console.log(await userRepo.inserir("Baloso Silva","quatro"));
+    console.log(await userRepo.inserir("Miguel da rocha","sete"));
+    console.log(await userRepo.mostrarTodos());
+}
+
+main()
