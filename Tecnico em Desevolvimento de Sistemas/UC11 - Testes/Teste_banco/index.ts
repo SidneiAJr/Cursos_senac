551ae17fad0893c84948c6cc803f3f1e618cf9d3
Sid Jr <sidalbertok@gmail.com>
2026-03-25 16:07:04 -0300

    Update index.ts

--- Tecnico em Desevolvimento de Sistemas/UC11 - Testes/Teste_banco/index.ts
+++ Tecnico em Desevolvimento de Sistemas/UC11 - Testes/Teste_banco/index.ts
@@ -5,6 +5,8 @@ async function main() {
     console.log(await userRepo.inserir("Daniel","20"));
     console.log(await userRepo.inserir("Baloso Silva","quatro"));
     console.log(await userRepo.inserir("Miguel da rocha","sete"));
+    console.log(await userRepo.delete(1));
+    console.log(await userRepo.update(1,"pedro","vinte"));
     console.log(await userRepo.mostrarTodos());
 }
 
