9b9a3a45e9b211b1b475aff946afb6a2bca598ee
Sid Jr <sidalbertok@gmail.com>
2026-03-25 14:36:33 -0300

    Create user-integration.test.ts

---
+++ Tecnico em Desevolvimento de Sistemas/UC11 - Testes/Teste_banco/user-integration.test.ts
@@ -0,0 +1,27 @@
+import { conn } from "../../config/database";
+import { UsuarioReposity } from "../../config/repositories/user-repository";
+
+describe("Teste de Integração de Usuario",()=>{
+    const repo = new UsuarioReposity();
+    beforeAll(async()=>{
+        await conn.execute('CREATE TABLE IF NOT EXISTS minha_tabela (id int primary key auto_increment not null, nome varchar(50) not null, idade varchar(50) not null);');
+    });
+    beforeEach(async()=>{
+        await conn.execute('Delete from minha_tabela;')
+    })
+    test("Deve criar um usuario",async()=>{
+        const user = await repo.inserir("Daniel","Seis"); 
+        expect (user).not.toBeUndefined;
+        expect (user.id).not.toBeNull;
+        expect (user.nome).toBe("Daniel");
+        expect (user.idade).toBe("Trinta");
+    })
+    test("Deve Apagar um Usuario",async()=>{
+        const user2 = await repo.delete(1); 
+        expect (user2).not.toBeNull;
+    })
+     test("Deve fazer Update do Usuario",async()=>{
+        const user3 = await repo.update(1,"Pedro Mesa","Vinte"); 
+        expect (user3).not.toBeNull;
+    })
+});
