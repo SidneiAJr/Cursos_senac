906a20bc602b10dc264746dbadbbd8daf8b300d9
Sid Jr <sidalbertok@gmail.com>
2026-04-09 14:56:08 -0300

    Update Pascoa.js

--- Tecnico em Desevolvimento de Sistemas/UC2 - JS/Pascoa.js
+++ Tecnico em Desevolvimento de Sistemas/UC2 - JS/Pascoa.js
@@ -1,3 +1,4 @@
+// Desafio 1  | Pàscoa
 function sumEven(numbers) {
     let sum = 0;
 
@@ -14,3 +15,23 @@ function sumEven(numbers) {
 
 
 console.log(sumEven([1, 2, 3, 4, 5, 6]));
+
+// Desafio 2 | Pascoa
+
+var result = 1;
+
+
+function multiplyArray(arr) {
+
+    arr =0;
+
+    for (let i = 0; i < arr.length; i++) {
+        result *= arr[i];
+    }
+
+
+    return result;
+}
+
+
+console.log(multiplyArray([1, 2, 3, 4]));
