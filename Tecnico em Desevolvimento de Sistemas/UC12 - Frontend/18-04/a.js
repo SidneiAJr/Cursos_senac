810f61ae0be740a9362ea7b758c810449afa0ab7
Sid Jr <sidalbertok@gmail.com>
2026-04-22 16:19:11 -0300

    Update a.js

--- Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/18-04/a.js
+++ Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/18-04/a.js
@@ -1,5 +1,15 @@
-const fundo = document.getElementById("t")
+const texto = document.getElementById('titulo')
 
 function modoNoturno(){
       alert("Acertou!");
+      texto.textContent = "Acertou"
+      texto.style.color = "green"
+      document.body.style.backgroundImage = "url('https://static.todamateria.com.br/upload/54/f2/54f271481875c-planetas-do-sistema-solar.jpg')";
 } 
+
+function erro(){
+    texto.textContent = "Erro!"
+    texto.style.color = "red"
+    document.body.style.backgroundImage = "url('https://i.makeagif.com/media/4-04-2025/3dXXV6.gif')";
+    document.body.style.filter = "brightness(1.5)";
+}
