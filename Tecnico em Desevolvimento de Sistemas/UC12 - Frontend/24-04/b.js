e495b38a3149225f3b633968ed0a66733e98fa9a
Sid Jr <sidalbertok@gmail.com>
2026-04-24 14:20:49 -0300

    Update b.js

--- Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/24-04/b.js
+++ Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/24-04/b.js
@@ -1,1 +1,22 @@
+const titulo = document.getElementById("a")
+const botaoadd = document.getElementById("add")
+const botaodelete = document.getElementById("remover")
+const botaotitulo = document.getElementById("titulo")
+const lista = document.getElementById("lista")
 
+function mudatexto(){
+    titulo.textContent = "Hello Word"
+}
+
+function addclass(){
+}
+
+function addlista(){}
+
+
+function realtime(){
+    input.addEventListener("input",(event)=>{
+    const valor = event.target.value
+    titulo.textContent = valor;
+})
+}
