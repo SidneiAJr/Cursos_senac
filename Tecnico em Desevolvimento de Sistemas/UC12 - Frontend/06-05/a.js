7843e52dcb6a8c7dc9fc2154c459a73c1fb3f177
Sid Jr <sidalbertok@gmail.com>
2026-05-06 13:38:52 -0300

    Create a.js

---
+++ Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/06-05/a.js
@@ -0,0 +1,30 @@
+const texto = document.getElementById("texto");
+const classe2 = document.querySelector(".exc2")
+const remover = document.getElementById("remover")
+const info = document.getElementById("info")
+const saida = document.getElementById("Saida")
+
+function one(){
+    texto.textContent = "Exercicio 1 | Criar um botão que muda o texto de um título | Alterado"
+}
+
+function double(){
+   classe2.classList.toggle("Ativo");
+}
+
+function tres(){
+    const input = document.getElementById("inputTexto");
+    const lista = document.getElementById("lista");
+    const novoItem = document.createElement("li");
+    novoItem.textContent = input.value;
+    lista.appendChild(novoItem);
+    input.value = ""; 
+}
+
+function quatro(){
+   remover.remove();
+}
+
+info.addEventListener("input",function(){
+    saida.textContent = info.value;
+})
