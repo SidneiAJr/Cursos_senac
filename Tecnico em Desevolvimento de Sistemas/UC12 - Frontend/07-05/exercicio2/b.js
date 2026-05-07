89c41ace29c5a699d25b40b91997482cb233a04e
Sid Jr <sidalbertok@gmail.com>
2026-05-07 15:31:18 -0300

    Update b.js

--- Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/07-05/exercicio2/b.js
+++ Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/07-05/exercicio2/b.js
@@ -3,16 +3,23 @@ const botaoRemove = document.getElementById("remove");
 const container = document.getElementById("container");
 
 let contador = 0;
+let numero = 0;
+
 
 botaoAdd.addEventListener("click", () => {
     const divNova = document.createElement("div");
+    if(numero===0){
+        contador = numero;
+    }
     divNova.classList.add("bola"); 
     contador += 1;
+    numero ++;
     divNova.textContent = contador;
     container.appendChild(divNova);
 
     divNova.addEventListener('click',()=>{
      divNova.remove();
+     numero --;
     })
     container.appendChild(div);
     
