3c72802a8f556f5e7df33afe9b5a2aa48421b34f
Sid Jr <sidalbertok@gmail.com>
2026-05-13 16:42:52 -0300

    Create index.tsx

---
+++ Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/13-05-React Native/index.tsx
@@ -0,0 +1,18 @@
+// Importando os comantes princiais que preciamos para contuir a tela
+import {View,Text,StyleSheet,Image} from "react-native";
+
+// Essa é a primeira tela do app
+// Ela mostra uma imagem e dois botoes
+// Ir login ou cadastro
+
+export default function IndexScree(){
+  return(
+    <View style={styles.container}>
+     <Image 
+     source={require("../../assets/images/")}>
+      style={styles.imagem}
+     resizemode='container'
+     </Image>
+    </View>
+  )
+}
