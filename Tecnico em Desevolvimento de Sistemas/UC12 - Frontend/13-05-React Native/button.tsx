b5833dd95e867817c3b4705ed5a26678bca31ac3
Sid Jr <sidalbertok@gmail.com>
2026-05-13 15:49:03 -0300

    Create button.tsx

---
+++ Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/13-05-React Native/button.tsx
@@ -0,0 +1,14 @@
+import {Pressable,Text,StyleSheet} from "react-native";
+
+type props={
+    title:string //Texto que aparece no botão
+    onPress:() => void //Função executada ao clicar
+}
+
+/*
+Aqui Vamos criar um botão
+Assim vitamos repetir codigo varias telas
+*/
+
+export default{}
+
