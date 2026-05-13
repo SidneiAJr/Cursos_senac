814fa36e98b5ca1631a04d5f8dae39f6ef33b0fa
Sid Jr <sidalbertok@gmail.com>
2026-05-13 17:01:07 -0300

    Update index.tsx

--- Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/13-05-React Native/index.tsx
+++ Tecnico em Desevolvimento de Sistemas/UC12 - Frontend/13-05-React Native/index.tsx
@@ -1,5 +1,7 @@
 // Importando os comantes princiais que preciamos para contuir a tela
 import {View,Text,StyleSheet,Image} from "react-native";
+import CustomButtom from "@/components/ui/Button";
+import { router } from "expo-router";
 
 // Essa é a primeira tela do app
 // Ela mostra uma imagem e dois botoes
@@ -13,6 +15,31 @@ export default function IndexScree(){
      style={styles.imagem}
      resizeMode='contain'
      />
+     <Text style={styles.title}>Hydro RS | Não Rolo</Text>
+     <CustomButtom
+      title = 'Ir para Cadastro'
+      onPress ={()=>router.push('/register')}
+     />
+     <CustomButtom
+      title = 'Ir para Login'
+      onPress ={()=>router.push('/Login')}
+     />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title:{
+
+  },
+  subtitle:{
+
+  },
+  imagem:{
+
+  },
+  container:{
+
+  }
+
+})
