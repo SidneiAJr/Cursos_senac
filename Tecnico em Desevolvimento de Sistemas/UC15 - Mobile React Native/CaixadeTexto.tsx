6398191435cdec15a55a788fa38c505aeca4d11c
Sid Jr <sidalbertok@gmail.com>
2026-05-28 16:38:19 -0300

    Create CaixadeTexto.tsx

---
+++ Tecnico em Desevolvimento de Sistemas/UC15 - Mobile React Native/CaixadeTexto.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+
+const CaixadeTexto = () => {
+  return (
+    <View>
+     <TextInput style={styles.caixa} placeholder ='Escreva seu nome'/>
+    </View>
+  )
+}
+
+export default CaixadeTexto
+
+const styles = StyleSheet.create({
+    caixa:{
+         color: 'white',
+        fontWeight : 'bold',
+        fontSize: 30,
+        backgroundColor: 'red',
+        borderRadius: 30,
+        width: 400,
+        height: 50
+    }
+})
