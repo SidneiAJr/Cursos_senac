006900d6231bb9dfb79cda6120a0f1857b47e7e2
Sid Jr <sidalbertok@gmail.com>
2026-05-28 16:54:44 -0300

    Update CaixacomScroll.tsx

--- Tecnico em Desevolvimento de Sistemas/UC15 - Mobile React Native/CaixacomScroll.tsx
+++ Tecnico em Desevolvimento de Sistemas/UC15 - Mobile React Native/CaixacomScroll.tsx
@@ -2,28 +2,29 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const CaixacomScroll = () => {
-  return (
-    <ScrollView style={styles.caixa}>
-        <Text>1</Text>
-        <Text>2</Text>
-        <Text>3</Text>
-        <Text>4</Text>
-        <Text>5</Text>
-        <Text>6</Text>
-        <Text>7</Text>
-        <Text>8</Text>
-        <Text>9</Text>
-        <Text>10</Text>
-    </ScrollView>
-  )
+    return (
+        <ScrollView style={styles.caixa}>
+            <Text>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vel, quod incidunt maiores totam architecto, neque facere ipsum consequuntur quam earum. Cumque consequuntur, fugit a dignissimos odio dolore delectus autem.</Text>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vel, quod incidunt maiores totam architecto, neque facere ipsum consequuntur quam earum. Cumque consequuntur, fugit a dignissimos odio dolore delectus autem.</Text>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vel, quod incidunt maiores totam architecto, neque facere ipsum consequuntur quam earum. Cumque consequuntur, fugit a dignissimos odio dolore delectus autem.</Text>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vel, quod incidunt maiores totam architecto, neque facere ipsum consequuntur quam earum. Cumque consequuntur, fugit a dignissimos odio dolore delectus autem.</Text>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vel, quod incidunt maiores totam architecto, neque facere ipsum consequuntur quam earum. Cumque consequuntur, fugit a dignissimos odio dolore delectus autem.</Text>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vel, quod incidunt maiores totam architecto, neque facere ipsum consequuntur quam earum. Cumque consequuntur, fugit a dignissimos odio dolore delectus autem.</Text>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vel, quod incidunt maiores totam architecto, neque facere ipsum consequuntur quam earum. Cumque consequuntur, fugit a dignissimos odio dolore delectus autem.</Text>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vel, quod incidunt maiores totam architecto, neque facere ipsum consequuntur quam earum. Cumque consequuntur, fugit a dignissimos odio dolore delectus autem.</Text>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vel, quod incidunt maiores totam architecto, neque facere ipsum consequuntur quam earum. Cumque consequuntur, fugit a dignissimos odio dolore delectus autem.</Text>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus vel, quod incidunt maiores totam architecto, neque facere ipsum consequuntur quam earum. Cumque consequuntur, fugit a dignissimos odio dolore delectus autem.</Text>
+        </ScrollView>
+    )
 }
 
 export default CaixacomScroll
 
 const styles = StyleSheet.create({
-    caixa:{
-         color: 'white',
-        backgroundColor: 'red',
-        fontWeight: 'bold'
+    caixa: {
+        color: 'white',
+        fontWeight: 'bold',
+        backgroundColor: 'rgba(255, 255, 255, 0.62)'
     }
 })
