08f63119b0155e0b676d9648f52aba0a1990f759
Sid Jr <sidalbertok@gmail.com>
2026-06-09 14:27:39 -0300

    Update server.ts

--- Tecnico em Desevolvimento de Sistemas/UC13 - Backend/09-06/server.ts
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/09-06/server.ts
@@ -6,69 +6,91 @@ const PORT = 3000;
 
 dotenv.config();
 const app: Express = express();
+
 const connection = mysql.createConnection({ 
     host:  'localhost',
     user:  'root',
     password:  'root',
-    database:  'teste'
+    database:  'd'
 });
 
+app.use(express.json()); 
 
-app.get('/testes', (req: Request, res: Response) => {
-    const sql = `SELECT * FROM [sua tabela aqui]`;
+app.get('/teste', (req: Request, res: Response) => {
+    const sql = `SELECT * FROM teste`;
     connection.query(sql, (err, results) => {
+        try {
         if (err) {
             return res.status(500).json({ erro: err.message });
         }
         const quantidade = results.length; 
         res.json({mensagem: `Quantidade de Registros: ${quantidade}`});
+        } catch (error) {
+            console.error("Erro Não foi possivel consultar o Banco!")
+        }
     });
 });
 
 app.post('/testes', (req: Request, res: Response) => {
-    const { nome, email } = req.body;
-    const sql = `INSERT INTO [sua tabela aqui] (nome, email) VALUES (?, ?)`;
-    connection.query(sql, [nome, email], (err, result) => {
+    const { nome, idade,cidade } = req.body;
+    const sql = `INSERT INTO teste (nome, idade,cidade) VALUES (?, ?,?)`;
+    connection.query(sql, [nome,idade,cidade], (err, result) => {
+        try {
         if (err) {
             return res.status(500).json({ erro: err.message });
         }
         const linhasAfetadas = result.affectedRows;  // Quantas linhas foram inseridas
         const novoId = result.insertId;  
         res.status(201).json({ mensagem: `Quantidade de Linhas Afetadas: ${linhasAfetadas} | Quantidade de Registros: ${novoId}` });
+        } catch (error) {
+            console.error("Erro Não foi possivel consultar o Banco!")
+        }
     });
 });
 
 
 app.put('/testes/:id', (req: Request, res: Response) => {
     const { id } = req.params;
-    const { nome, email } = req.body;
-    const sql = `UPDATE [sua tabela aqui] SET nome = ?, email = ? WHERE id = ?`;
-    connection.query(sql, [nome, email, id], (err, result) => {
+   const { nome, idade,cidade } = req.body;
+    const sql = `UPDATE teste SET nome = ?, idade = ?, cidade=? WHERE id = ?`;
+    connection.query(sql, [nome,idade,cidade ,id], (err, result) => {
+        try {
         if (err) {
-            return res.status(500).json({ erro: err.message });
+        return res.status(500).json({ erro: err.message });
         }
         if (result.affectedRows === 0) {
             return res.status(404).json({ erro: 'Registro não encontrado' });
         }
-         res.json({ mensagem: "Atualizado com sucesso!" });
+         res.json({ mensagem: "Atualizado com sucesso!" }); 
+        } catch (error) {
+              console.error("Erro Não foi possivel consultar o Banco!")
+        }
+       
     });
 });
 
 
 app.delete('/testes/:id', (req: Request, res: Response) => {
     const { id } = req.params;
-    const sql = `DELETE FROM [sua tabela aqui] WHERE id = ?`;
+    const sql = `DELETE FROM teste WHERE id = ?`;
     connection.query(sql, [id], (err, result) => {
-        if (err) {
+        try {
+        const linhasAfetadas = result.affectedRows;     
+        const linhasEncontradas = result.changedRows; 
+            if (err) {
             return res.status(500).json({ erro: err.message });
         }
         if (result.affectedRows === 0) {
             return res.status(404).json({ erro: 'Registro não encontrado' });
         }
          res.json({ mensagem: `Deletado com sucesso | Linhas Afetadas: ${linhasAfetadas} | Linhas Encotradas ${linhasEncontradas}` });
+        } catch (error) {
+             console.error("Erro Não foi possivel consultar o Banco!")
+        }
     });
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
