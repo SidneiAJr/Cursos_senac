00964be2e3bc8b6a070f057d5976e1121b10988d
Sid Jr <sidalbertok@gmail.com>
2026-06-11 20:30:21 -0300

    Create database.ts

---
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/Por fora/database.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import { Livros } from './Livro';
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "lib",
+    synchronize: false, // cuidado em produção!
+    logging: true,
+    entities: [Livros],
+});
