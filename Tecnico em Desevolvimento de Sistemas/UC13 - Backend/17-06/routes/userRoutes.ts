59c7617cf37e2240f0fae6297e19af70fab5ad36
Sid Jr <sidalbertok@gmail.com>
2026-06-17 16:29:44 -0300

    Update userRoutes.ts

--- Tecnico em Desevolvimento de Sistemas/UC13 - Backend/17-06/routes/userRoutes.ts
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/17-06/routes/userRoutes.ts
@@ -2,9 +2,21 @@ import { Router } from "express";
 import { UsuarioController } from "../controllers/UserController";
 
 const router = Router();
+const controller = new UsuarioController();
 
-const controller =new UsuarioController();
+// Criar usuário
+router.post("/usuarios", controller.createUsuario.bind(controller));
 
+// Listar todos
+router.get("/usuarios", controller.listarUsuarios.bind(controller));
 
+// Atualizar completo
+router.put("/usuarios/:id", controller.updateUsuario.bind(controller));
+
+// Atualizar parcial (só email)
+router.patch("/usuarios/:id", controller.updateParcialUsuario.bind(controller));
+
+// Deletar
+router.delete("/usuarios/:id", controller.deletarUsuario.bind(controller));
 
 export default router;
