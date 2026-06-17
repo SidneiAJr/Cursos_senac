8d086eec888d06ad2fb1e5dddb4705595d14075b
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-06-17 14:52:33 -0300

    Fix: Conserta dia 17-06

---
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/17-06/controllers/UserController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from "express";
+import { UsuarioService } from "../services/UserService";
+
+export class UsuarioController {
+    private service = new UsuarioService();
+
+    async createUsuario(req: Request, res: Response) {
+        try {
+            const { email, password } = req.body;
+
+            if (!email || !password) {
+                return res.status(400).json({ mensagem: "Email e senha são obrigatórios" });
+            }
+
+            await this.service.create(email, password);
+            return res.status(201).json({ mensagem: "Usuário criado com sucesso!" });
+
+        } catch (error: any) {
+            return res.status(500).json({ mensagem: "Erro interno: " + error.message });
+        }
+    }
+
+    async updateUsuario(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);  
+            const { email, password } = req.body;
+
+            if (!email || !password) {
+                return res.status(400).json({ mensagem: "Email e senha são obrigatórios" });
+            }
+
+            await this.service.update(id, email, password);
+            return res.status(200).json({ mensagem: "Usuário atualizado com sucesso" });
+
+        } catch (error: any) {
+            return res.status(500).json({ mensagem: "Erro interno: " + error.message });
+        }
+    }
+
+    async updateParcialUsuario(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const { email } = req.body;
+
+            if (!email) {
+                return res.status(400).json({ mensagem: "Email é obrigatório" });
+            }
+
+            await this.service.updateParcial(id, email);
+            return res.status(200).json({ mensagem: "Usuário atualizado parcialmente" });
+
+        } catch (error: any) {
+            return res.status(500).json({ mensagem: "Erro interno: " + error.message });
+        }
+    }
+
+    async listarUsuarios(req: Request, res: Response) {
+        try {
+            const usuarios = await this.service.findAll();
+            return res.status(200).json(usuarios);
+        } catch (error: any) {
+            return res.status(500).json({ mensagem: "Erro interno: " + error.message });
+        }
+    }
+
+    async deletarUsuario(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+
+            if (!id || isNaN(id)) {
+                return res.status(400).json({ mensagem: "ID inválido" });
+            }
+
+            await this.service.delete(id);
+            return res.status(200).json({ mensagem: "Usuário deletado com sucesso" });
+
+        } catch (error: any) {
+            return res.status(500).json({ mensagem: "Erro interno: " + error.message });
+        }
+    }
+}
---
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/17-06/routes/userRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { UsuarioController } from "../controllers/UserController";
+
+const router = Router();
+
+const controller =new UsuarioController();
+
+
+
+export default router;
