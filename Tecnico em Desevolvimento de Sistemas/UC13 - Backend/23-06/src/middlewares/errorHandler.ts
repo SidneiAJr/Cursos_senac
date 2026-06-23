ccf979f99edb45849e96b8b91aa73182d4d41f5b
Sid Jr <sidalbertok@gmail.com>
2026-06-23 16:18:12 -0300

    Delete Tecnico em Desevolvimento de Sistemas/UC13 - Backend/23-06/src/middlewares/errorHandler.ts

--- Tecnico em Desevolvimento de Sistemas/UC13 - Backend/23-06/src/middlewares/errorHandler.ts
+++
@@ -1,5 +0,0 @@
-export const errorMiddleware = (err,req,res,next) => {
-  const status = err.statusCode || 500;
-  const isOperation = err.isOperation || false
-  res.status(status).json({mensagem: isOperation? err.mensagem: "Erro inesperado. Teste Novamente mais Tarde"});
-};
