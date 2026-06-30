baf629f3e9205a455cc93b812d029239cbbe421e
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-06-30 17:11:29 -0300

    Feat: Adiciona exercicio 1 do typeorm

--- Tecnico em Desevolvimento de Sistemas/UC13 - Backend/30-06 Exercicio 1 TypeORM/src/config/database.ts
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/30-06 Exercicio 1 TypeORM/src/config/database.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import * as dotenv from 'dotenv'
+import { Jogador } from "../models/Jogador";
+import { Selecao } from "../models/Selecao";
 
 dotenv.config()
 
@@ -14,6 +16,6 @@ export const AppDataSource = new DataSource({
     password: DB_PASSWORD,
     database: DB_NAME,
     synchronize: true, 
-    logging: false,
-    entities: [],
+    logging: true,
+    entities: [Jogador,Selecao],
 });
