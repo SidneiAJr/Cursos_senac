8ea295b676d48d8cb5858b79f1fdc97db6412d3f
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-07-02 17:04:07 -0300

    Feat: Adiciona Util omitpassword

---
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/02-07 TypeORM & Zod sem Comentarios/src/controllers/UsuarioController.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import { UserService } from "../services/UserService";
+
+
+export class UserController{
+
+   async list(req:Request,res:Response,next:NextFunction){
+       try {
+        const users = await UserService.ListAll()
+        return res.json(users)
+       } catch (error) {
+        next(error)
+       }
+   }
+
+   async getById(req:Request,res:Response,next:NextFunction){
+       try {
+        const id = Number(req.body.id)
+        const user = await UserService.getByID(id)
+        return res.json(user)
+       } catch (error) {
+        next(error)
+       }
+   }
+
+   async create(req:Request,res:Response,next:NextFunction){
+      try {
+        const {nome,email,password} = req.body
+        const user = UserService.create({nome,email,password})
+        return res.status(201).json(user)
+      } catch (error) {
+         next(error)
+      }
+   }
+}
