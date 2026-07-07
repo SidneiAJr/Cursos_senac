dda7f6ea9d800706830f378ff9bb1552d40ec1d8
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-07-07 16:27:43 -0300

    Fix: Conserta 07-07

--- Tecnico em Desevolvimento de Sistemas/UC13 - Backend/07-07 TypeORM/src/models/Usuario.ts
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/07-07 TypeORM/src/models/Usuario.ts
@@ -1,5 +1,18 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 
+@Entity("Usuario")
 export class Usuario {
+@PrimaryGeneratedColumn()
+id:number
+
+@Column({length:100, nullable:true , default: 0, type: "varchar"})
+
+@Column({length:100, nullable:true , default: 0, type: "varchar"})
+
+@Column({length:100, nullable:true , default: 0, type: "varchar"})
+
+@Column({length:100, nullable:true , default: 0, type: "varchar"})
+
+@Column({length:100, nullable:true , default: 0, type: "varchar"})
 
 }
--- Tecnico em Desevolvimento de Sistemas/UC13 - Backend/07-07 TypeORM/src/repositories/UsuarioRepository.ts
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/07-07 TypeORM/src/repositories/UsuarioRepository.ts
@@ -1,9 +1,23 @@
+import { AppDataSource } from "../config/database";
 import { Usuario } from "../models/Usuario";
 
+const repo = AppDataSource.getRepository(Usuario)
+
 export class UsuarioRepository {
-    async create(){}
-    async update(){}
-    async delete(){}
-    async listId(){}
-    async listall(){}
+    async create(data:{}){
+       const user = repo.create(Usuario)
+       return repo.save(user)
+    }
+    async update(user:Usuario){
+        return repo.save(Usuario)
+    }
+    async delete(id:number){
+       return repo.delete(id)
+    }
+    async listId(id:number){
+       return repo.findOne({})
+    }
+    async listall(){
+      return repo.find();
+    }
 }
