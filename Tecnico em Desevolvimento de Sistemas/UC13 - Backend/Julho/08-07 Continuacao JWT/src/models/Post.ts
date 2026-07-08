7afddcaddf02286caa803cbff3c4e3da74cea0e8
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-07-08 16:05:50 -0300

    feat: Adiciona pasta 08-07

---
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/Julho/08-07 Continuacao JWT/src/models/Post.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./Usuario"; 
+
+@Entity('posts')  
+export class Post {
+    @PrimaryGeneratedColumn()  
+    id!: number;
+
+    @Column({ type: "varchar", length: 100, nullable: false })  
+    title!: string;
+
+    @ManyToOne(() => User, (user) => user.posts, {nullable:false})  
+    user!: User;
+}
