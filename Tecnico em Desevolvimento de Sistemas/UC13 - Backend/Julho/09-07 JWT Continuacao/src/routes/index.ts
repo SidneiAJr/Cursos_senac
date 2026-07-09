d8eb22afae5f8ab34413889f7220164c127ac637
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-07-09 14:38:21 -0300

    Fix: 09-07

--- Tecnico em Desevolvimento de Sistemas/UC13 - Backend/Julho/09-07 JWT Continuacao/src/routes/index.ts
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/Julho/09-07 JWT Continuacao/src/routes/index.ts
@@ -3,6 +3,7 @@ import { UserController } from "../controllers/UsuarioController";
 import { validateUser } from "../middlewares/validateUser";
 import { PostController } from "../controllers/PostController";
 import { AuthController } from "../controllers/AuthController";
+import { authMiddleware } from "../middlewares/auth-middleware";
 
 const routes = Router()
 
@@ -11,18 +12,18 @@ const postController = new PostController();
 const authController = new AuthController()
 
 // Rotas dos Usuarios
-routes.get('/users',userController.list.bind(userController))
-routes.get('/users/:id',userController.getById.bind(userController))
+routes.get('/users',authMiddleware,userController.list.bind(userController))
+routes.get('/users/:id',authMiddleware,userController.getById.bind(userController))
 routes.post('/users',validateUser,userController.create.bind(userController))
-routes.delete("/users/:id",userController.delete.bind(userController))
-routes.patch("/users/:id",userController.Update.bind(userController))
+routes.delete("/users/:id",authMiddleware,userController.delete.bind(userController))
+routes.patch("/users/:id",authMiddleware,userController.Update.bind(userController))
 routes.post("/login",authController.login.bind(authController))
 
 // Rotas dos Post
-routes.get("/posts", postController.list.bind(postController));
-routes.get("/posts/:id", postController.getById.bind(postController));
-routes.post("/posts", postController.create.bind(postController));
-routes.delete("/posts/:id",postController.delete.bind(postController))
-routes.patch("/posts/:id",postController.Update.bind(postController))
+routes.get("/posts", authMiddleware,postController.list.bind(postController));
+routes.get("/posts/:id", authMiddleware,postController.getById.bind(postController));
+routes.post("/posts", authMiddleware,postController.create.bind(postController));
+routes.delete("/posts/:id",authMiddleware,postController.delete.bind(postController))
+routes.patch("/posts/:id",authMiddleware,postController.Update.bind(postController))
 
 export default routes
