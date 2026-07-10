6cecd6e7edc6fa4fa1bb53d8ff6352dc572cba6f
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-07-10 14:33:43 -0300

    Feat: adiciona 10-07

--- Tecnico em Desevolvimento de Sistemas/UC13 - Backend/Julho/10-07 JWT Continuacao 2/src/schemas/UsuarioSchemas.ts
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/Julho/10-07 JWT Continuacao 2/src/schemas/UsuarioSchemas.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const createUserScheme = z.object({
     nome: z.string().min(1, "Nome é obrigatório"),
     email: z.string().email("Email inválido").min(1, "Email é obrigatório"),
-    password: z.string().min(8, "Senha precisa ter no mínimo 4 caracteres")
+    password: z.string().min(8, "Senha precisa ter no mínimo 8 caracteres")
 });
 
 export const updateUserScheme = createUserScheme.partial();
