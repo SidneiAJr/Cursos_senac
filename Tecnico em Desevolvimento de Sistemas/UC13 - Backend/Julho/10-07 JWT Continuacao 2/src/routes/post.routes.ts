bb1877818cb947a6d2a93471040832efa5400d21
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-07-10 17:10:00 -0300

    Fix: conserta 10-07

--- Tecnico em Desevolvimento de Sistemas/UC13 - Backend/Julho/10-07 JWT Continuacao 2/src/routes/post.routes.ts
+++ Tecnico em Desevolvimento de Sistemas/UC13 - Backend/Julho/10-07 JWT Continuacao 2/src/routes/post.routes.ts
@@ -12,5 +12,6 @@ postRouter.get("/posts/:id", authMiddleware,postController.getById.bind(postCont
 postRouter.post("/posts",authMiddleware,validatePost,postController.create.bind(postController));
 postRouter.delete("/posts/:id",authMiddleware,postController.delete.bind(postController))
 postRouter.patch("/posts/:id",authMiddleware,postController.Update.bind(postController))
+postRouter.get("/myposts",authMiddleware,postController.listMyPost.bind(postController))
 
 export default postRouter
