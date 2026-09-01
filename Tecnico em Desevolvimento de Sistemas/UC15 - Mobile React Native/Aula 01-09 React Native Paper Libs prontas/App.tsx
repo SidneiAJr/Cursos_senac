53190a0431af165371b4c4b8b6c2fbe015d6e1c8
Sid Jr <sidalbertok@gmail.com>
2026-09-01 16:23:57 -0300

    Update App.tsx

--- Tecnico em Desevolvimento de Sistemas/UC15 - Mobile React Native/Aula 01-09 React Native Paper Libs prontas/App.tsx
+++ Tecnico em Desevolvimento de Sistemas/UC15 - Mobile React Native/Aula 01-09 React Native Paper Libs prontas/App.tsx
@@ -5,16 +5,58 @@ import ImgFundo from './imgfundo';
 
 const LeftContent = (props) => <Avatar.Icon {...props} icon="shopping" />;
 
-// ADICIONEI ID AQUI
 const lista = [
-  { id: '1', nome: 'Produto1', preco: 200, descricao: 'Produto 1' },
-  { id: '2', nome: 'Produto2', preco: 2000, descricao: 'Produto 2' },
-  { id: '3', nome: 'Produto3', preco: 250, descricao: 'Produto 3' },
-  { id: '4', nome: 'Produto4', preco: 200, descricao: 'Produto 4' },
-  { id: '5', nome: 'Produto5', preco: 2000, descricao: 'Produto 5' },
-  { id: '6', nome: 'Produto6', preco: 20000, descricao: 'Produto 6' },
-  { id: '7', nome: 'Produto7', preco: 200000, descricao: 'Produto 7' },
+  {
+    id: '1',
+    nome: 'Produto1',
+    preco: 200,
+    descricao: 'Produto 1',
+    imagem: 'https://super.abril.com.br/wp-content/uploads/2003/08/everest.jpg?crop=1&resize=1212,909',
+  },
+  {
+    id: '2',
+    nome: 'Produto2',
+    preco: 2000,
+    descricao: 'Produto 2',
+    imagem: 'https://quatrorodas.abril.com.br/wp-content/uploads/2016/11/5723fab70e21634575011f03qr-635-gb-01-eps.jpeg?quality=70&strip=all&strip=all',
+  },
+  {
+    id: '3',
+    nome: 'Produto3',
+    preco: 250,
+    descricao: 'Produto 3',
+    imagem: 'https://quatrorodas.abril.com.br/wp-content/uploads/2016/11/57c7621c0e2163719706709eopala_01-psd.jpeg?quality=70&strip=all&strip=all',
+  },
+  {
+    id: '4',
+    nome: 'Produto4',
+    preco: 200,
+    descricao: 'Produto 4',
+    imagem: 'https://s2-autoesporte.glbimg.com/9Q1J2vdLHYloHXL_B4GWv74jaeM=/0x0:620x413/600x0/smart/filters:gifv():strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/D/H/x6ZIJIT2KwZABkJrFyFA/2016-08-19-monza08.jpg',
+  },
+  {
+    id: '5',
+    nome: 'Produto5',
+    preco: 2000,
+    descricao: 'Produto 5',
+    imagem: 'https://cdn.motor1.com/images/mgl/e3VwM/s1/justica-brasileira-processa-gm-por-omissao-de-recall-do-vectra-em-1998.jpg',
+  },
+  {
+    id: '6',
+    nome: 'Produto6',
+    preco: 20000,
+    descricao: 'Produto 6',
+    imagem: 'https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=375,h=257,fit=crop/A3Q2pPVOoRUNv05q/chatgpt-image-26_09_2025-13_47_45-mePgnxjE6pcKX5Rj.png',
+  },
+  {
+    id: '7',
+    nome: 'Planeta | Venus',
+    preco: 2,
+    descricao: 'Defeito Precisa Terraformar',
+    imagem: 'https://content.nationalgeographic.pt/medio/2024/02/28/planeta-venus_cab4dd46_240228170348_1280x1280.jpg',
+  },
 ];
+
 
 export default function App() {
   const [busca, setBusca] = useState('');
@@ -35,7 +77,7 @@ export default function App() {
       <Card.Content>
         <Text variant="bodyMedium" style={styles.descricao}>{item.descricao}</Text>
       </Card.Content>
-      <Card.Cover source={{ uri: 'https://picsum.photos/300' }} />
+     <Card.Cover source={{ uri: item.imagem }} />
       <Card.Actions>
         <Button labelStyle={styles.botaoTexto}>Limpar</Button>
         <Button labelStyle={styles.botaoTexto}>Comprar</Button>
@@ -45,7 +87,7 @@ export default function App() {
 
   return (
     <ImgFundo
-      colors={['rgba(4, 4, 189, 0.8)', 'rgba(51, 7, 7, 0.8)', 'rgba(204, 96, 33, 0.8)', 'rgba(223, 222, 222, 0.8)']}
+      colors={['rgba(4, 4, 189, 0.8)', 'rgba(51, 7, 7, 0.8)', 'rgba(0, 0, 0, 0.8)', 'rgba(100, 17, 17, 0.8)']}
     >
       <View style={styles.container}>
         <TextInput
